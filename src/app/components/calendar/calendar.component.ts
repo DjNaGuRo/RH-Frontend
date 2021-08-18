bb32908a65a5be02b75136f7263ac0aa6d640d54
totara-thib <totara.thib@gmail.com>
2021-08-18 17:14:00 +0200

    Navigation beetween month and years

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -7,31 +7,43 @@ import * as moment from 'moment';
   styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent implements OnInit {
-
+  currentMonth!:number;
+  currentYear = moment().year()
   daysOffMonth?:string[]
   constructor() { }
 
   ngOnInit(): void {
-    this.getDaysArrayByMonth()
-    console.log( this.daysOffMonth);
-    
+    this.getDaysArrayByMonth(moment().month())
   }
-  
-   getDaysArrayByMonth() {
-     var daysInMonth = moment([2021, 1]).daysInMonth();
+
+   getDaysArrayByMonth(monthChoice:number) {
+
+     if (monthChoice < 0 ) {
+       monthChoice = 11
+       this.currentYear -= 1
+     }
+     if (monthChoice > 11 ) {
+      monthChoice = 0
+      this.currentYear += 1
+    }
+     var daysInMonth = moment([this.currentYear, monthChoice]).daysInMonth();
      var arrDays = [];
-     
+
      while(daysInMonth) {
-       var current = moment().date(daysInMonth);
+       var current = moment([this.currentYear, monthChoice]).date(daysInMonth);
        arrDays.push(current);
        daysInMonth--;
       }
     let daysOffMonthFormat:string[] = []
+    let currentMonth:number
+    currentMonth = parseInt(arrDays[0].format("MM"))
+
     arrDays.forEach(function(item) {
-      daysOffMonthFormat.push(item.format("DD/MM"));
+      daysOffMonthFormat.push(item.locale("fr").format("dd \n DD/MM"));
     })
+    this.currentMonth = currentMonth
     daysOffMonthFormat.reverse()
     this.daysOffMonth = daysOffMonthFormat
-    
+
   }
 }
