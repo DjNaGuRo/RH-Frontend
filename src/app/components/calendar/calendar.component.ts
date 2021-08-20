f7c642cd0036f7b70ebcff6356c5ac77432c07ea
totara-thib <totara.thib@gmail.com>
2021-08-20 13:55:48 +0200

    First version of differents functions  done, need to upgrade the design

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -3,7 +3,9 @@ import * as moment from 'moment'
 import { Collaborator } from 'src/app/model/collaborator'
 import { DayOff } from 'src/app/model/dayOff'
 import { UserService } from 'src/app/services/user.service'
-
+interface CollaboratorCalendar extends Collaborator {
+    daysOffMonth:(DayOff | null)[]
+}
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
@@ -15,15 +17,20 @@ export class CalendarComponent implements OnInit {
   currentYear = moment().year()
   daysOffMonth?: string[]
   collaborators?: Collaborator[]
+  daysOffToInclude = new Map<number, DayOff>()
+  collaboratorsCalendar:CollaboratorCalendar[] = []
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-    this.getDaysArrayByMonth(moment().month())
-    this.userService.getCollaborator().subscribe(collaborators => this.collaborators = collaborators)
+    this.userService.getCollaborator().subscribe(collaborators => {
+      this.collaborators = collaborators
+      this.getDaysArrayByMonth(moment().month())
+    })
+
   }
 
   getDaysArrayByMonth(monthChoice: number) {
-
+    this.collaboratorsCalendar = []
     if (monthChoice < 0) {
       monthChoice = 11
       this.currentYear -= 1
@@ -40,32 +47,56 @@ export class CalendarComponent implements OnInit {
       arrDays.push(current)
       daysInMonth--
     }
+    arrDays.reverse()
     let daysOffMonthFormat: string[] = []
     let currentMonth: number
     currentMonth = parseInt(arrDays[0].format("MM"))
     this.currentMonthLetter = arrDays[0].locale("fr").format("MMMM")
-    arrDays.forEach(function (item) {
-      daysOffMonthFormat.push(item.locale("fr").format("DD/MM"))
-    })
+    if (this.collaborators) {
+      for (const collaborator of this.collaborators) {
+        const collabCalendar:CollaboratorCalendar = {
+          ...collaborator,
+          daysOffMonth : []
+        }
+        for (const day of arrDays) {
+          let dayOffFind = false
+          for (const dayOff of collaborator.daysOffs) {
+            if (dayOff.startDate === day.locale("fr").format("DD/MM/YYYY")) {
+              collabCalendar.daysOffMonth.push(dayOff)
+              dayOffFind = true
+            }
+          }
+          if (!dayOffFind) {
+            collabCalendar.daysOffMonth.push(null)
+          }
+        }
+        this.collaboratorsCalendar.push(collabCalendar)
+      }
+      for (const day of arrDays) {
+        daysOffMonthFormat.push(day.locale("fr").format("dd \n DD/MM"))
+      }
+    }
+    console.log(this.collaboratorsCalendar);
+
     this.currentMonth = currentMonth
-    daysOffMonthFormat.reverse()
+    daysOffMonthFormat
     this.daysOffMonth = daysOffMonthFormat
 
   }
   testCollaborator() {
     console.log(this.collaborators)
   }
-  compareDate(date: string): boolean {
+  compareDate(date: string): DayOff | null {
     if (this.collaborators) {
       for (const collaborator of this.collaborators) {
         for (const dayOff of collaborator.daysOffs) {
           if (dayOff.startDate.includes(date)) {
-            return true;
+            return dayOff;
           }
         }
       }
     }
-    return false
+    return null
   }
   // console.log(dayOff)
   // console.log(date)
