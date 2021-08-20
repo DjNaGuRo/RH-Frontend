b313d841b593ccb6c52c4bdb0bb793b3d48c99fb
Tubeuf <charles.tubeuf@hotmail.fr>
2021-08-20 10:11:09 +0200

    reset form when submit/Cross click/clicking on a backdrop

--- src/app/components/day-off-form/day-off-form.component.ts
+++ src/app/components/day-off-form/day-off-form.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DaysOff } from '../../model/daysOff';
 import { NgbDatepickerConfig, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
-import {AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators} from '@angular/forms';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { NgbDate, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgbDateStructAdapter } from '@ng-bootstrap/ng-bootstrap/datepicker/adapters/ngb-date-adapter';
 
@@ -15,36 +15,38 @@ imports: [NgbModule]
 
 export class DayOffFormComponent {
   closeResult = '';
-  minEndDate!:NgbDateStruct; 
+  minEndDate!: NgbDateStruct;
   dayOffForm!: FormGroup;
 
   current = new Date();
-  minDate:NgbDateStruct = { year: this.current.getFullYear(), month: 
-    this.current.getMonth() + 1, day: this.current.getDate()+1 };
+  minDate: NgbDateStruct = {
+    year: this.current.getFullYear(), month:
+      this.current.getMonth() + 1, day: this.current.getDate() + 1
+  };
 
-  constructor(private modalService: NgbModal,private fb: FormBuilder, private config: NgbDatepickerConfig) {
-    
+  constructor(private modalService: NgbModal, private fb: FormBuilder, private config: NgbDatepickerConfig) {
+
     console.log(this.dayOffForm);
     config.outsideDays = 'hidden';
   }
-  
-  
+
+
   ngOnInit(): void {
     this.dayOffForm = this.fb.group(
       {
-        'dayOffType': ['', [Validators.required]], 
-        'startDate': ['', [Validators.required]], 
-        'endDate': ['', [Validators.required]] 
-      },{
-        updateOn: 'blur',
-      });
-      this.dayOffForm.get('startDate')?.valueChanges.subscribe(startDate=>this.minEndDate=startDate);
+        'dayOffType': ['', [Validators.required]],
+        'startDate': ['', [Validators.required]],
+        'endDate': ['', [Validators.required]]
+      }, {
+      updateOn: 'blur',
+    });
+    this.dayOffForm.get('startDate')?.valueChanges.subscribe(startDate => this.minEndDate = startDate);
 
   }
 
 
   open(content: any) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
@@ -60,103 +62,15 @@ export class DayOffFormComponent {
       return `with: ${reason}`;
     }
   }
-
+  resetDayOffForm() {
+    this.dayOffForm.reset();
+  }
   // test appel fonction avec le bouton submit
   verifForm() {
-      console.log("La vérification se lance : ");
-      console.log(this.dayOffForm.get('startDate')?.value);
-      console.log(this.dayOffForm.get('endDate')?.value);
-      console.log(this.dayOffForm.get('dayOffType')?.value);
+    console.log("La vérification se lance : ");
+    console.log(this.dayOffForm.get('startDate')?.value);
+    console.log(this.dayOffForm.get('endDate')?.value);
+    console.log(this.dayOffForm.get('dayOffType')?.value);
+    this.resetDayOffForm();
   }
 }
-
-// exemple de ROSSI
-
-
-// @Component({
-//   selector: 'app-nouveau-collegue-reactive-form',
-//   templateUrl: './nouveau-collegue-reactive-form.component.html',
-//   styleUrls: ['./nouveau-collegue-reactive-form.component.css'],
-// })
-// export class NouveauCollegueReactiveFormComponent implements OnInit {
-//   formCollegue!: FormGroup;
-
-//   constructor(private fb: FormBuilder, private dataSrv: DataService, private router: Router) {
-//   }
-
-//   ngOnInit(): void {
-//     this.formCollegue = this.fb.group(
-//       {
-//         'pseudo': ['', [Validators.required, Validators.min(2)], [this.validerPseudo.bind(this)]],
-//         'nom': ['', [Validators.required, Validators.min(2)]],
-//         'prenom': ['', [Validators.required, Validators.min(2)]],
-//         'photo': ['', [Validators.required, Validators.min(2)]],
-//       }, {
-//         // validateur qui s'applique à l'ensemble du formulaire
-//         validators: [this.validerNomEtPrenomNonIdentique.bind(this)],
-//         updateOn: 'blur'
-//       }
-//     );
-//   }
-
-//   validerPseudo(control: AbstractControl): Observable<ValidationErrors | null> {
-
-//     return this.dataSrv
-//       .findCollegue(control.value)
-//       .pipe(
-//         // si la requête est ok => le pseudo saisi existe => ko pour nous
-//         map(() => <ValidationErrors>{ pseudoExistant: true }),
-//         // si la requête échoue => pseudo n'existe pas => ok pour nous
-//         catchError(() => of(null))
-//       );
-
-//   }
-
-
-
-//   validerNomEtPrenomNonIdentique(control: FormControl): ValidationErrors | null {
-//     if (!control.value.nom  || control.value.nom != control.value.prenom) {
-//       return null;
-//     }
-//     return {nomPrenomIdentique: true};
-//   }
-
-//   creerDaysOff() {
-//     let collegue: Collegue = this.formCollegue.value;
-
-//     this.dataSrv.creerCollegue(collegue)
-//       .subscribe(() => this.redirectAccueil());
-//   }
-
-//   get isPseudoInvalid() {
-//     return (
-//       this.formCollegue.get('pseudo')?.invalid &&
-//       this.formCollegue.get('pseudo')?.dirty
-//     );
-//   }
-
-//   get isNomInvalid() {
-//     return (
-//       this.formCollegue.get('nom')?.invalid &&
-//       this.formCollegue.get('nom')?.dirty
-//     );
-//   }
-
-//   get isPrenomInvalid() {
-//     return (
-//       this.formCollegue.get('prenom')?.invalid &&
-//       this.formCollegue.get('prenom')?.dirty
-//     );
-//   }
-
-//   get isPhotoInvalid() {
-//     return (
-//       this.formCollegue.get('photo')?.invalid &&
-//       this.formCollegue.get('photo')?.dirty
-//     );
-//   }
-
-//   redirectAccueil() {
-//     this.router.navigateByUrl("")
-//   }
-// }
