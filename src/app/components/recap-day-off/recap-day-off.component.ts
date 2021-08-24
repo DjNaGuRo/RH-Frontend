5332d91f6f23ef752eef98af7462905c9946eb92
Tubeuf <charles.tubeuf@hotmail.fr>
2021-08-24 16:29:59 +0200

    en cours

--- src/app/components/recap-day-off/recap-day-off.component.ts
+++ src/app/components/recap-day-off/recap-day-off.component.ts
@@ -5,11 +5,31 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './recap-day-off.component.html',
   styleUrls: ['./recap-day-off.component.scss']
 })
+
+interface Ligne {
+  dayOffType: string;
+  pris: number;
+  restant: number;
+}
+
 export class RecapDayOffComponent implements OnInit {
 
+  let lignes!: Ligne[] = [
+    {
+      dayOffType: 'RTTE',
+      pris: 5,
+      restant: 12
+    },
+    {
+      dayOffType: 'RTTE',
+      pris: 5,
+      restant: 12
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
-}
+
+} // fin de la class
