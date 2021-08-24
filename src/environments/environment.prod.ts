390047d2ad35c0742736728616cc51024c6359cf
guillaumedussart <dussartguillaume.dev@gmail.com>
2021-08-24 10:30:35 +0200

    change env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
-const baseUrlApi = 'https://rhmanagement.herokuapp.com/'
+const baseUrlApi = 'https://rhmanagement.herokuapp.com/';
+
 export const environment = {
   production: false,
   baseUrlApi,
