c0e25f988db9730f2e271a3d0b093fdb5d507ac1
Tubeuf <charles.tubeuf@hotmail.fr>
2021-08-30 14:41:48 +0200

    recap en cours de construction

--- src/app/components/recap-day-off/recap-day-off.component.ts
+++ src/app/components/recap-day-off/recap-day-off.component.ts
@@ -37,24 +37,52 @@ export class RecapDayOffComponent implements OnInit {
     this.userService.getCollaborator().subscribe(collaborator => {
     this.collaborator = collaborator;
     this.daysOff = collaborator.daysOffs;
-    for (const day of this.daysOff) {
-      let startDateString = day.startDate
-      let momentVariableStart = moment(startDateString, 'DD/MM/YYYY');
-      let stringvalue = momentVariableStart.format('YYYY-MM-DD');
-      let dateStartDate = new Date(stringvalue);
-      let endDateString = day.startDate
-      let momentVariableEnd = moment(endDateString, 'DD/MM/YYYY');
-      let stringvalue2 = momentVariableEnd.format('YYYY-MM-DD');
-      let dateEndDate = new Date(stringvalue2);
-      console.log("nombre de jour de congé : ");
-      console.log(momentVariableStart.diff(momentVariableEnd));
-
-    }
+    // let total days
+   
     console.log ("recherche de daysOffs : ");
   console.log(this.collaborator?.daysOffs);
     })
 
 }
+
+calculateDaysOff (daysOff: DayOff[]){
+for (const day of daysOff) {
+  let startDateString = day.startDate
+  let momentVariableStart = moment(startDateString, 'DD/MM/YYYY');
+  let stringvalue = momentVariableStart.format('YYYY-MM-DD');
+  // let dateStartDate = new Date(stringvalue);
+  let endDateString = day.endDate
+  let momentVariableEnd = moment(endDateString, 'DD/MM/YYYY');
+  let stringvalue2 = momentVariableEnd.format('YYYY-MM-DD');
+  // let dateEndDate = new Date(stringvalue2);
+  // let daysCount = momentVariableEnd.diff(momentVariableStart, 'days')+1;
+  let workingDays =  this.calculateBusinessDays(momentVariableStart, momentVariableEnd);
+}
+}
+
+calculateBusinessDays(d1: moment.Moment, d2: moment.Moment) {
+  const days = d2.diff(d1, "days") + 1;
+  let newDay: any = d1.toDate(),
+  workingDays: number = 0,
+  sundays: number = 0,
+  saturdays: number = 0;
+  for (let i = 0; i < days; i++) {
+      const day = newDay.getDay();
+      newDay = d1.add(1, "days").toDate();
+      const isWeekend = ((day % 6) === 0);
+      if (!isWeekend) {
+          workingDays++;
+      } 
+      else {
+          if (day === 6) saturdays++;
+          if (day === 0) sundays++;
+      }
+  }
+      console.log("Total Days:", days, "workingDays", workingDays, "saturdays", saturdays, "sundays", sundays);
+  return workingDays;
+}
+
+
   lignes: Ligne[] = [
     {
       dayOffType: 'RTTE',
