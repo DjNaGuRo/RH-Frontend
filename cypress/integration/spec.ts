c88c934c9bcaffba27b2d95bb929ebe15f325e58
DJOUNANG NANA Guy Rostan <guyrostan_nana@yahoo.fr>
2021-12-23 16:32:20 +0100

    fix: add shortcut command start:ci in package.json

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,7 +1,16 @@
 describe('My First Test', () => {
   it('Visit the login page', () => {
-      cy.visit('login')
+      cy.visit('/')
 
-      cy.contains('Angular') //.click('topLeft')
+/*       cy.get('#email')
+        .type("fake@go2scale.com")
+        .should('have.value', "fake@go2scale.com")
+
+      cy.get('#password')
+        .type("123fake")
+        .should('have.value', "123fake")
+
+      cy.get('#login-form button')//.click() */
+
   })
 })
